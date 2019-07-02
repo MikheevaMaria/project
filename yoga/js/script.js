62367fac539e3a24d80b886a98669352411b1503
Mikheeva Maria <51912821+MikheevaMaria@users.noreply.github.com>
2019-07-02 10:52:30 +0500

    Update script.js

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -34,7 +34,7 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    let deadline = '2019-06-21';
+    let deadline = '2019-07-13';
 
     function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -80,4 +80,245 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     setClock('timer', deadline);
+
+   //Form
+
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро с вами свяжемся',
+        failure: 'Ошибка'
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = document.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+        statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST'), 'server.php';
+        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+        
+        let formData = new FormData(form);
+        let obj = {};
+        formData.forEach(function(value, key) {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+        request.send(json);
+
+        //readystatechange ~ load
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+        for (let i = 0; i < input.length ; i++) {
+            input[i].value = '';
+        }
+    });
+
 });
+
+/*-----------------------------*/
+
+/*function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function() {
+        console.log('Hello!' + this.name);
+    }
+}
+
+User.prototype.exit = function(name) {
+    console.log('Пользователь ' + this.name + ' ушел');
+}
+
+let ivan = new User('Ivan', 25),
+    alex = new User('Alex', 20);
+
+console.log(ivan);
+console.log(alex);
+
+alex.exit();*/
+
+/*-----------------------------*/
+
+/*class user {
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+        this.human = true;
+    }
+    hello() {
+        console.log('Hello!' + this.name);
+    }
+    exit() {
+        console.log('Пользователь ' + this.name + ' ушел');
+    }
+}
+
+let ivan = new User('Ivan', 25),
+    alex = new User('Alex', 20);
+
+console.log(ivan);
+console.log(alex);
+
+alex.exit();*/
+
+/*'use strict';
+
+function showThis(a, b) {
+    console.log(this);
+
+    function sum() {
+        console.log(this);
+        return a + b;
+    }
+    
+    console.log(sum());
+}
+
+showThis(4, 5);
+showThis(5, 5);*/
+
+// window/undefined
+
+/*let obj = {
+    a: 20,
+    b: 15,
+    sum : function() {
+        console.log(this);
+
+        function shout() {
+            console.log(this);
+        }
+        shout();
+    }
+};
+
+obj.sum();*/
+
+// this = объект
+
+// Конструктор (new) - this = новый созданный объект
+
+/*let user = {
+    name: "John"
+};
+
+function sayName(surname) {
+    console.log(this);
+    console.log(this.name + surname);
+}
+
+console.log(sayName.call(user, 'Smith'));
+console.log(sayName.apply(user, ['Snow']));
+
+function count(number) {
+    return this * number;
+}
+
+let double = count.bind(2);
+console.log(double(3));
+console.log(double(10));*/
+
+// Указания конкретного контекста - call, apply, bind
+
+/*let btn = document.querySelector('button');
+
+btn.addEventListener('click', function() {
+    console.log(this);
+    this.style.backgroundColor = 'red';
+    function showThis() {
+        console.log(this);
+    }
+
+    showThis();
+});*/
+
+/*--------------------------------------------*/
+/*let name = 'Ivan',
+    age = 30,
+    mail = 'ex@mail.ru';
+
+//интерполяция
+document.write(`Пользователю ${name} ${age} лет. Его почтовый адрес: ${mail}`);
+
+function makeArray() {
+    var items = [];
+
+    for (let i = 0; i < 10; i++) {
+        var item = function() {
+            console.log(i);
+        };
+        items.push(item);
+    }
+
+    return items;
+}
+
+var arr = makeArray();
+
+arr[1]();
+arr[3]();
+arr[7]();
+
+let fun = () => {
+    console.log(this);
+};
+
+//fun();
+
+let obj = {
+    number: 5,
+    sayNumber: function() {
+        let say = () => {
+            console.log(this);
+        };
+        say();
+    }
+}
+
+obj.sayNumber();
+
+let btn = document.querySelector('button');
+
+btn.addEventListener('click', function() {
+    let show = () => {
+        console.log(this);
+    };
+    show();
+});
+
+function calcOrDouble(number, basis = 2) {
+    //basis = basis || 2; ES5
+    console.log(number*basis);
+}
+
+calcOrDouble(3, 5);
+calcOrDouble(6);
+
+class Rectangle {
+    constructor(height, width = 20) {
+        this.height = height;
+        this.width = width;
+    }
+    calcArea() {
+        return this.height*this.width;
+    }
+}
+
+const square = new Rectangle(10, 10);
+console.log(square.calcArea());
+*/
