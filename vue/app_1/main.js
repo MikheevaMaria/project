41a40bf114489c8569c7bdb066454857bb9d3a84
Mikheeva Maria <51912821+MikheevaMaria@users.noreply.github.com>
2019-07-04 12:43:43 +0500

    Create main.js

---
+++ vue/app_1/main.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: '#app',
+
+    data: {
+        submissions: Seed.submissions
+    },
+
+    components: {
+        'submission-component': submissionComponent
+    },
+
+    computed: {
+        sortedSubmissions() {
+            return this.submissions.sort((a, b) => b.votes - a.votes);
+        }
+    },
+
+});
+
+//Vue.component('submission-component', submissionComponent);
